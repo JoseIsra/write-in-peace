d4fe42a0e358fc55a8b4ff9bf903b58fc810bce4
JoseIsra <israelmyname@gmail.com>
2022-08-17 12:46:02 -0500

    ⚗️ (cookie): cookie experiment
    cookie experiment to get token

--- src/utils/jsonToken.js
+++ src/utils/jsonToken.js
@@ -24,7 +24,7 @@ const createTokenRefresher = (payload, res) => {
       expiresIn,
     });
     res.cookie("refresherToken", refreshToken, {
-      httpOnly: true,
+      // httpOnly: true,
       secure: !(process.env.MODE === "developer"),
       expires: new Date(Date.now() + expiresIn * 1000),
     });
